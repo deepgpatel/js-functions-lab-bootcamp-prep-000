2c456f5c737a3fbde992c11b99fde4daf8d2460e
Deep Patel <deepgpatel93@gmail.com>
2018-01-30 00:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(you)
+{
+  return (`Happy holidays, ${name}!`)
+}
+
+function happyHolidaysTo(Diwali, Utkala)
+{
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(Diwali, Ten)
+{
+  return (`It's ${days} days until ${holiday}!`)
+}
